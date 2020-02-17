09dd72878568c1914a1213632903ff0a6d808103
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:30:27 +0200

    Created a script where the login will be specified in upper case during registration

--- registration_with_upper_case_login_test.js
+++ registration_with_upper_case_login_test.js
@@ -6,4 +6,20 @@ Feature('registration_with_upper_case_login');
 
 Scenario('test something', (I) => {
 
+	I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('registration_with_upper_case_login.png');
+        I.wait(1);
 });
