1c3ea56fb3d1300e1e292a87a60bcf315702edc2
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:10:56 +0200

    Created variables login, password, email.  The email variable has an invalid value.

--- registration_with_incorrect_email_test.js
+++ registration_with_incorrect_email_test.js
@@ -1,3 +1,7 @@
+const login = 'qatestlogin';
+const password = 'aiKeichil4';
+const email = 'some_email_box_qatesting.ml';
+
 Feature('registration_with_incorrect_email');
 
 Scenario('test something', (I) => {
