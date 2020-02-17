015a6e889fad7d293df74be2ef8da39d50cdb2c0
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:02:45 +0200

    Added variables email, login, password. The email parameter has an empty value for the email field.

--- registration_with_empty_field_email_test.js
+++ registration_with_empty_field_email_test.js
@@ -1,5 +1,9 @@
+const login = 'qatestlogin1';
+const password = 'aiKeichil4';
+const email = 'some_email_box_qatesting.ml';
+
 Feature('registration_with_empty_field_email');
 
-Scenario('test something', (I) => {
+Scenario('registration_with_empty_field_email', (I) => {
 
 });
