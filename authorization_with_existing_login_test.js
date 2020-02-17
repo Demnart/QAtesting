e09817eda28669322a57825c4cb86a705ca46156
Rogov Artiom <rogov@reg.ru>
2020-02-17 04:31:36 +0200

    Added variable login, password, email. In the value of the login variable, the already existing user in the system is specified.

--- authorization_with_existing_login_test.js
+++ authorization_with_existing_login_test.js
@@ -1,5 +1,10 @@
+const login = 'qatestlogin';
+const password = 'aiKeichil4';
+const email = 'some_email_box1@qatesting.ml';
+
 Feature('authorization_with_existing_login');
 
-Scenario('test something', (I) => {
+Scenario('authorization_with_existing_login', (I) => {
+
 
 });
