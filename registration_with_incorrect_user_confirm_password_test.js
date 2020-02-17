b1748183c655e7acc8ae513f4d68164d7f8f756f
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:27:16 +0200

    Added variables login, password, email. In the email variable, the value is in uppercase.

--- registration_with_incorrect_user_confirm_password_test.js
+++ registration_with_incorrect_user_confirm_password_test.js
@@ -8,4 +8,21 @@ Feature('registration_with_incorrect_user_confirm_password');
 
 Scenario('registration_with_incorrect_user_confirm_password', (I) => {
 
+        I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' , password);
+        I.wait(1);
+        I.fillField('user_confirm_password', check_password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('success_registration.png');
+        I.wait(1);
+
 });
--- registration_with_upper_case_login_test.js
+++ registration_with_upper_case_login_test.js
@@ -1,3 +1,7 @@
+const login = 'QATESTLOGIN10';
+const password = 'aiKeichil4';
+const email = 'some_email_box2@qatesting.ml';
+
 Feature('registration_with_upper_case_login');
 
 Scenario('test something', (I) => {
