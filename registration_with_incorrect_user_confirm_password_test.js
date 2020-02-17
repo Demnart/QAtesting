9fe2e2a8281a0a1638e2003b6fd79437b560a0f1
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:19:19 +0200

    Created 4 variables login,password,check_password,email. The check_password variable specifies a password that does not match the password specified in the password variable.

--- registration_with_incorrect_user_confirm_password_test.js
+++ registration_with_incorrect_user_confirm_password_test.js
@@ -1,5 +1,11 @@
+const login = 'qatestlogin';
+const password = 'aiKeichil4';
+const check_password = '1234';
+
+const email = 'some_email_box@qatesting.ml';
+
 Feature('registration_with_incorrect_user_confirm_password');
 
-Scenario('test something', (I) => {
+Scenario('registration_with_incorrect_user_confirm_password', (I) => {
 
 });
