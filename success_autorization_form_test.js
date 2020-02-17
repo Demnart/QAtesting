c3e5af6684aaccda9dfa1ab6f9af407ebeef9db4
Rogov Artiom <rogov@reg.ru>
2020-02-17 03:28:56 +0200

    Written a script for successful registration on the site.

--- success_autorization_form_test.js
+++ success_autorization_form_test.js
@@ -10,5 +10,17 @@ Scenario('successful registration', (I) => {
 	I.wait(1);
 	I.click('Registration');
 	I.wait(1);
+	I.fillField('user_login', login);
+	I.wait(1);
+	I.fillField('user_email', email);
+	I.wait(1);
+	I.fillField('user_pass' ,password);
+	I.wait(1);
+	I.fillField('user_confirm_password', password);
+	I.wait(1);
+	I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+	I.wait(1);
+	I.saveScreenshot('success_registration.png');
+	I.wait(1);
 
 });
