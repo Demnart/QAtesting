0621d28ab4faa09ad7625497da44454278be1f83
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:48:51 +0200

    Created three variables with values in upper case

--- registration_with_uppercase_all_fields_test.js
+++ registration_with_uppercase_all_fields_test.js
@@ -1,3 +1,8 @@
+const login = 'ADFQER';
+const password = 'ASDFJQEWR';
+const email = 'QATESTIN@QATESTING.ML';
+
+
 Feature('registration_with_uppercase_all_fields');
 
 Scenario('test something', (I) => {
