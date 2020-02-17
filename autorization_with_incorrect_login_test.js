dbf01a4b3004250f6fff981cdd95756e6c8cf7d8
Rogov Artiom <rogov@reg.ru>
2020-02-17 04:10:36 +0200

    Added variable login, password, email. Login is not specified correctly.

--- autorization_with_incorrect_login_test.js
+++ autorization_with_incorrect_login_test.js
@@ -1,5 +1,12 @@
+const login = '1234<>?*?"'\"||/';
+const password = 'aiKeichil4';
+const email = 'some_email_box@qatesting.ml';
+
+
 Feature('autorization_with_incorrect_login');
 
 Scenario('test something', (I) => {
 
 });
+
+
