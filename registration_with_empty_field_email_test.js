b49cf0a8e6995d21843e5787aa97bc24d2a650b8
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:08:01 +0200

    Written a script where the email field remains empty during registration.

--- registration_with_empty_field_email_test.js
+++ registration_with_empty_field_email_test.js
@@ -1,9 +1,25 @@
 const login = 'qatestlogin1';
 const password = 'aiKeichil4';
-const email = 'some_email_box_qatesting.ml';
+const email = ' ';
 
 Feature('registration_with_empty_field_email');
 
 Scenario('registration_with_empty_field_email', (I) => {
 
+	I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('empty_field_email.png');
+        I.wait(1);
 });
