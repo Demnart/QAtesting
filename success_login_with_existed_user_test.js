622d1b64b7ff1084f57b91c1d342a3261f9580d5
Rogov Artiom <rogov@reg.ru>
2020-02-17 04:03:24 +0200

    wrote a script that performs authorization in the private office based on the data already created.

--- success_login_with_existed_user_test.js
+++ success_login_with_existed_user_test.js
@@ -3,6 +3,23 @@ const password = 'aiKeichil4';
 
 Feature('success_login_with_existed_user');
 
-Scenario('test something', (I) => {
+Scenario('success login', (I) => {
+
+	I.amOnPage('/');
+	I.wait(1);
+	I.click('My Account');
+	I.wait(1);
+	I.fillField('username', login);
+	I.wait(1);
+	I.fillField('password', password);
+	I.wait(1);
+	I.click('//*[@id="rememberme"]');
+	I.wait(1);
+	I.click('login');
+	I.wait(1);
+	I.saveScreenshot('success_login.png');
+	I.wait(1);
+	I.click('Logout');
+
 
 });
