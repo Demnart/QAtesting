57b0d1065a7df30aa5c423b6c0765a084a3eff9c
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:44:59 +0200

    Created a script in which the email is uppercase

--- registration_with_uppercase_email_test.js
+++ registration_with_uppercase_email_test.js
@@ -5,6 +5,22 @@ const email = 'QATESTING123@QATESTING.ML';
 
 Feature('registration_with_uppercase_email');
 
-Scenario('test something', (I) => {
+Scenario('registration_with_uppercase_email', (I) => {
 
+	        I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('registration_with_uppercase_email.png');
+        I.wait(1);
 });
