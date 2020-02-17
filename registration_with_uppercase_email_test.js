dc53cd1083bd62f11ed658c46f70c623b26b5ddf
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:42:27 +0200

    Created 3 variables login, password, email. In the email variable, the value is uppercase.

--- registration_with_uppercase_email_test.js
+++ registration_with_uppercase_email_test.js
@@ -1,3 +1,8 @@
+const login = 'qatestlogin1234';
+const password = 'aiKeichil4';
+const email = 'QATESTING123@QATESTING.ML';
+
+
 Feature('registration_with_uppercase_email');
 
 Scenario('test something', (I) => {
