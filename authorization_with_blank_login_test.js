4ab81ccd1670b5d9eed454f7ab2f3ab0ac23ccd3
Rogov Artiom <rogov@reg.ru>
2020-02-17 04:23:18 +0200

    Created variables login, password, email. The value of the variable login is an empty field.

--- authorization_with_blank_login_test.js
+++
@@ -1,5 +0,0 @@
-Feature('authorization_with_blank_login');
-
-Scenario('test something', (I) => {
-
-});
---
+++ authorization_with_empty_login_test.js
@@ -0,0 +1,11 @@
+const login = '';
+const password = 'aiKeichil4';
+const email = 'some_email_box1@qatesting.ml';
+
+
+Feature('authorization_with_empty_login');
+
+Scenario('authorization_with_empty_login', (I) => {
+
+
+});
