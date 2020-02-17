1d3574aca2378f6dfbfb7d1965735a97928623a3
Rogov Artiom <rogov@reg.ru>
2020-02-17 04:25:43 +0200

    Written a script in which the registration will take place with an empty login field.

--- authorization_with_empty_login_test.js
+++ authorization_with_empty_login_test.js
@@ -7,5 +7,21 @@ Feature('authorization_with_empty_login');
 
 Scenario('authorization_with_empty_login', (I) => {
 
+	I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('empty_login_screen.png');
+        I.wait(1);
 
 });
