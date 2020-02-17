620ea2bc7f3ae1e88c7d9be39eeb1d2345138bcf
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:51:05 +0200

    Created a script that checks the form when all data is in upper case

--- registration_with_uppercase_all_fields_test.js
+++ registration_with_uppercase_all_fields_test.js
@@ -7,4 +7,20 @@ Feature('registration_with_uppercase_all_fields');
 
 Scenario('test something', (I) => {
 
+	I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('all_fields_uppercase.png');
+        I.wait(1);
 });
