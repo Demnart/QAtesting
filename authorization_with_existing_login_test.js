4db61987e36506de56c8ec9e160c849d8a55a4fa
Rogov Artiom <rogov@reg.ru>
2020-02-17 04:39:10 +0200

    Written a scenario where an attempt is made to register with an existing login

--- authorization_with_existing_login_test.js
+++ authorization_with_existing_login_test.js
@@ -6,5 +6,20 @@ Feature('authorization_with_existing_login');
 
 Scenario('authorization_with_existing_login', (I) => {
 
-
+	I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('existing_login.png');
+        I.wait(1);
 });
