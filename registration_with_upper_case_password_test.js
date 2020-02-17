ffea09cb1f01a890bb7e63a5be8a20b579cb207c
Rogov Artiom <rogov@reg.ru>
2020-02-17 05:38:44 +0200

    We have created a script where the upper case password will be specified during the registration.

--- registration_with_upper_case_password_test.js
+++ registration_with_upper_case_password_test.js
@@ -1,9 +1,24 @@
 const login = 'qatestlogin11';
 const password = 'AIHEICHI14';
-const email = 'some_email_box@qatesting.ml';
+const email = 'some_email_box44@qatesting.ml';
 
 Feature('registration_with_upper_case_password');
 
-Scenario('test something', (I) => {
-
+Scenario('registration_with_upper_case_password', (I) => {
+	I.amOnPage('/');
+        I.wait(1);
+        I.click('Registration');
+        I.wait(1);
+        I.fillField('user_login', login);
+        I.wait(1);
+        I.fillField('user_email', email);
+        I.wait(1);
+        I.fillField('user_pass' ,password);
+        I.wait(1);
+        I.fillField('user_confirm_password', password);
+        I.wait(1);
+        I.click('//*[@id="user-registration-form-5"]/form/div[2]/button');
+        I.wait(1);
+        I.saveScreenshot('registration_with_upper_case_password.png');
+        I.wait(1)
 });
