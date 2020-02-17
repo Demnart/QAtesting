cad1f974ceef5c27dd3e9ac93ead0d1958876c7e
Rogov Artiom <rogov@reg.ru>
2020-02-17 03:15:22 +0200

    Added the variables login, password, email. They store the values that will be used in the registration form.

--- success_autorization_form_test.js
+++ success_autorization_form_test.js
@@ -1,8 +1,14 @@
+const login = 'qatestlogin';
+const password = 'aiKeichil4';
+const email = 'some_email_box@qatesting.ml';
+
 Feature('autorization_form');
 
-Scenario('test something', (I) => {
+Scenario('successful registration', (I) => {
 
 	I.amOnPage('/');
-	I.wait(5);
+	I.wait(1);
+	I.click('Registration');
+	I.wait(1);
 
 });
